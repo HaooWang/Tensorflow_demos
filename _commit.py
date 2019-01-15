f0f7768c28a0247744f8a4697a0f7860f0246e66
HAO WANG <haowanghk@gmail.com>
2019-01-15 16:44:30 +0800

    commit 19

---
+++ _commit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/1/15 4:38 PM
+# @Author  : HaoWang
+# @Site    : 
+# @File    : _commit.py
+# @Software: PyCharm
+#
+# import tensorflow as tf
+#
+# hello = "hello world"
+#
+# with tf.Session() as sess:
+#     print(sess.run(hello))
