b1b3d92987dfa7a6a2979557f23e8fe9792928f6
HAO WANG <haowanghk@gmail.com>
2019-01-16 14:34:12 +0800

    tensorflow neural network frame

---
+++ forward_propagation/FP.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/1/16 12:50 PM
+# @Author  : HaoWang
+# @Site    : 
+# @File    : FP.py
+# @Software: PyCharm
+
+import tensorflow as tf
+
+with tf.Session() as sess:
+    print(sess.run())
