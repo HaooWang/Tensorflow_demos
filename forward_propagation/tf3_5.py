4b3ca9da930499d4ef076cee145198111c373b16
HAO WANG <haowanghk@gmail.com>
2019-01-16 15:00:18 +0800

    tensorflow neural network frame

--- forward_propagation/tf3_5.py
+++ forward_propagation/tf3_5.py
@@ -9,6 +9,7 @@ x = tf.placeholder(tf.float32, shape=(None, 2))
 w1= tf.Variable(tf.random_normal([2, 3], stddev=1, seed=1))
 w2= tf.Variable(tf.random_normal([3, 1], stddev=1, seed=1))
 
+
 #定义前向传播过程
 a = tf.matmul(x, w1)
 y = tf.matmul(a, w2)
